f7c959957fcc4c84bfe2572576b659f5d79d2c3c
austind9999 <austind9999@hotmail.com>
2019-12-17 16:43:46 -0500

    Update flairfixed.py

--- flairfixed.py
+++ flairfixed.py
@@ -46,7 +46,7 @@ class Bot(object):
         stream = subreddit.stream.submissions()
         for post in stream:
             if not post.link_flair_text: continue
-            if post.link_flair_text.lower() #== remove_flair.lower():
+            if post.link_flair_text.lower(): #== remove_flair.lower():
                 #print('removing {0}'.format(post.shortlink))
             report = {'reason': post.link_flair_text}
             self.handle_report(subreddit, report, post.link_flair_text.lower())
