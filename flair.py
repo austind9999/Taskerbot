0e747d81e87d216d839cdec79703945e83dc22b4
austind9999 <austind9999@hotmail.com>
2019-12-17 17:41:19 -0500

    Update flair.py

--- flair.py
+++ flair.py
@@ -1,18 +1,6 @@
 import praw
 import yaml
-import re
 from time import sleep
-from praw.models.reddit.submission import Submission
-
-if __name__ == '__main__':
-    with open('config.yaml') as config_file:
-        CONFIG = yaml.load(config_file)
-        CLIENT_ID = CONFIG['Client ID']
-        CLIENT_SECRET = CONFIG['Client Secret']
-        USERNAME = CONFIG['Username']
-        PASSWORD = CONFIG['Password']
-        SUBREDDITS = CONFIG['Subreddits']
-        USER_AGENT = CONFIG['User Agent']
 
 r = praw.Reddit(client_id=CLIENT_ID,
                 client_secret=CLIENT_SECRET,
@@ -74,11 +62,17 @@ def main():
             if post.link_flair_text.lower() == remove_flair11.lower():
                 print('removing {0}'.format(post.shortlink))
                 post.report('!rule 11')
-
     except Exception as e:
         print('### exception: {0}'.format(str(e)))
         sleep(60)
  
  
 if __name__ == '__main__':
-    main()
+    with open('config.yaml') as config_file:
+        CONFIG = yaml.load(config_file)
+        CLIENT_ID = CONFIG['Client ID']
+        CLIENT_SECRET = CONFIG['Client Secret']
+        USERNAME = CONFIG['Username']
+        PASSWORD = CONFIG['Password']
+        SUBREDDITS = CONFIG['Subreddits']
+        USER_AGENT = CONFIG['User Agent']
