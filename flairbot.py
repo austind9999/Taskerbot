bf79656726098ccb0cb9548b4882b099c867f29a
austind9999 <austind9999@hotmail.com>
2019-12-18 11:32:51 -0500

    Update flairbot.py

--- flairbot.py
+++ flairbot.py
@@ -203,7 +203,7 @@ class Bot(object):
 
                     self.log(subreddit, '\n\n{} removed {}'.format(
                         report['author'], permalink))
-        elif continue
+        continue
                 
     def run(self):
         while True:
