83259659e1cd39e394c5656a76059943638e4ad4
austind9999 <austind9999@hotmail.com>
2019-12-18 10:40:02 -0500

    Update flairbot.py

--- flairbot.py
+++ flairbot.py
@@ -49,7 +49,7 @@ class Bot(object):
 #        for post in self.r.subreddit(subreddit).submissions():
         stream = self.r.subreddit(subreddit).stream.submissions()
         for post in stream:
-            report = {'source': SubmissionFlair, 'reason': post.link_flair_text.lower()}
+            report = {'source': SubmissionFlair, 'reason': post.link_flair_text}
             self.handle_report(subreddit, report, post.link_flair_text())
         
     def handle_report(self, subreddit, report, target):
