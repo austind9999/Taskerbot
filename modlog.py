6d10b8e917df7bf93bafdd505a262a11ddc77129
austind9999 <austind9999@hotmail.com>
2019-12-21 00:12:30 -0500

    Update modlog.py

--- modlog.py
+++ modlog.py
@@ -73,7 +73,6 @@ class Bot(object):
                 print(log.target_fullname)
                 postname = log.target_fullname
                 print(postname)
-                print(postname.body)
                 print(postname.link_flair_text)
                     #report = {'source': postname, 'reason': postname.link_flair_text, 'author': 'Flair'}
                     #self.handle_report(subreddit, report, postname)
