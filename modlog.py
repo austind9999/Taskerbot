4d11e45827d44eb41e273565f12a5f98c21d66fe
austind9999 <austind9999@hotmail.com>
2019-12-21 00:47:58 -0500

    Update modlog.py

--- modlog.py
+++ modlog.py
@@ -78,7 +78,7 @@ class Bot(object):
                 a, s = s[:n], s[n:]
                 print(a)
                 print(s)
-                for reddit.submission(log.target_fullname.split('_')[1]):
+                for submission in reddit.submission(log.target_fullname.split('_')[1]):
                     print(submission.link_text_flair)
                     #report = {'source': postname, 'reason': postname.link_flair_text, 'author': 'Flair'}
                     #self.handle_report(subreddit, report, postname)
