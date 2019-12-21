0d29857ef5b9fa23f028d9da1c8b65e9926fde5b
austind9999 <austind9999@hotmail.com>
2019-12-20 23:38:10 -0500

    Update modlog.py

--- modlog.py
+++ modlog.py
@@ -62,7 +62,7 @@ class Bot(object):
     #Check Vars
     def check_flairs(self, subreddit):
         logging.info('Checking subreddit modlog: %s…', subreddit)
-        for log in self.r.subreddit(subreddit).mod.log(limit=5): 
+        for log in self.r.subreddit(subreddit).mod.log(limit=15): 
             if log is None:
                 continue
             if log.action == 'editflair':
