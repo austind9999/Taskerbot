6e1628d306c4b9fa4dfcbc3a5ed642ecc6d38959
austind9999 <austind9999@gmail.com>
2020-01-10 11:49:32 -0500

    Update dev.py

--- dev.py
+++ dev.py
@@ -46,8 +46,8 @@ class Bot(object):
         logging.info('Reasons loaded.')
         
     def test_perms(self, subreddit):
-        for mod in self.r.subreddit(subreddit).moderator():
-            print('{}: {}'.format(mod, mod.mod_permissions))
+        for moderator in reddit.subreddit('aww').moderators():
+            print('{}: {}'.format(moderator, moderator.mod_permissions))
         
     def check_flairs(self, subreddit):
         logging.info('Checking subreddit flairs: %s…', subreddit)
