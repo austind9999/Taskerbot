afc4e460590ff9e77b87df0b1ad939f8d94d2bc3
austind9999 <austind9999@gmail.com>
2020-01-10 13:27:12 -0500

    Update dev.py

--- dev.py
+++ dev.py
@@ -54,11 +54,9 @@ class Bot(object):
             self.r.subreddit(subreddit).wiki['taskerbot'].content_md))
         logging.info('Reasons loaded.')
         
-    #def check_perms(self, subreddit):
-    #    logging.info('Checking perms: %s...', subreddit)
-    #    for moderator in self.r.subreddit(subreddit).moderator():
-    #        print(moderator)
-    #        print('{}: {}'.format(moderator, moderator.mod_permissions))
+    def check_perms(self, subreddit):
+        logging.info('Checking perms: %s...', subreddit)
+        print(dict)
         
     def check_flairs(self, subreddit):
         logging.info('Checking subreddit flairs: %s…', subreddit)
@@ -214,10 +212,10 @@ class Bot(object):
             for subreddit in SUBREDDITS:
                 try:
                     self.check_comments(subreddit)
-                    self.check_flairs(subreddit)
-                    self.check_reports(subreddit)
-                    self.check_mail()
-                   # self.check_perms(subreddit)
+                   # self.check_flairs(subreddit)
+                   # self.check_reports(subreddit)
+                   # self.check_mail()
+                    self.check_perms(subreddit)
                 except Exception as exception:
                     logging.exception(exception)
             logging.info('Sleeping…')
