7cc022515c83f0363e50082b7d8b9db6d47fb69b
austind9999 <austind9999@gmail.com>
2020-01-10 11:53:07 -0500

    Update dev.py

--- dev.py
+++ dev.py
@@ -11,6 +11,7 @@ from praw.models.reddit.comment import Comment
 from praw.models.reddit.submission import Submission
 from praw.models.reddit.submission import SubmissionFlair
 from praw.models.reddit.subreddit import SubredditModeration
+from praw.models.reddit.subreddit import ModeratorRelationship
 import yaml
 
 
