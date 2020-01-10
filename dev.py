38cd4bb9e20aef10fecc9a1c6b49025281802f14
austind9999 <austind9999@gmail.com>
2020-01-10 12:08:54 -0500

    Update dev.py

--- dev.py
+++ dev.py
@@ -29,6 +29,9 @@ class Bot(object):
             sub['mods'] = list(mod.name for mod in
                                self.r.subreddit(subreddit).moderator())
             logging.info('Mods loaded: %s.', sub['mods'])
+            logging.info('Loading permissions...')
+            sub['perms'] = list(mod.permission for mod in
+                               self.r.subreddit(subreddit).moderator())
             logging.info('Loading reasons…')
             sub['reasons'] = yaml.load(html.unescape(
                 self.r.subreddit(subreddit).wiki['taskerbot'].content_md))
@@ -45,10 +48,6 @@ class Bot(object):
         sub['reasons'] = yaml.load(html.unescape(
             self.r.subreddit(subreddit).wiki['taskerbot'].content_md))
         logging.info('Reasons loaded.')
-        
-    def test_perms(self, subreddit):
-        mod.permissions for mod in self.r.subreddit(subreddit).moderator():
-            print('{}: {}'.format(mod, mod.permissions))
         
     def check_flairs(self, subreddit):
         logging.info('Checking subreddit flairs: %s…', subreddit)
@@ -206,8 +205,8 @@ class Bot(object):
                    # self.check_comments(subreddit)
                    # self.check_flairs(subreddit)
                    # self.check_reports(subreddit)
-                   # self.check_mail()
-                    self.test_perms(subreddit)
+                    self.check_mail()
+                   # self.test_perms(subreddit)
                 except Exception as exception:
                     logging.exception(exception)
             logging.info('Sleeping…')
